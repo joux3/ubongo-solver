754d75c15a217462fc7870c639b5da58581f567f
Antti Risteli <antti@timma.fi>
2019-12-24 22:34:44 +0200

    Updates

---
+++ precache-manifest.1b9ad498043835386be06c0c6fd6345d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "253d00f2cd37aae8857724aa18cffcd7",
+    "url": "/ubongo-solver/index.html"
+  },
+  {
+    "revision": "c5687c31f3c79e55a1f0",
+    "url": "/ubongo-solver/static/css/main.04e84cff.chunk.css"
+  },
+  {
+    "revision": "1ad0cc62c76378d6d4dc",
+    "url": "/ubongo-solver/static/js/2.d726cf9b.chunk.js"
+  },
+  {
+    "revision": "6aeb469c0c20fd3334c86de961c4692c",
+    "url": "/ubongo-solver/static/js/2.d726cf9b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c5687c31f3c79e55a1f0",
+    "url": "/ubongo-solver/static/js/main.ff336fcc.chunk.js"
+  },
+  {
+    "revision": "9073aa516f38c0f163db",
+    "url": "/ubongo-solver/static/js/runtime-main.de03ea41.js"
+  }
+]);
